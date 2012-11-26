dc815c022c5268d27f27d1c53c97a1877d43e7cd
saitoha <user@zuse.jp>
2012-11-26 21:12:58 +0900

    Use TFF 0.0.10

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(name                  = 'canossa',
       packages              = find_packages(exclude=[]),
       zip_safe              = True,
       include_package_data  = False,
-      install_requires      = ['tff ==0.0.9'],
+      install_requires      = ['tff >=0.0.10, <0.1.0'],
       entry_points          = """
                               [console_scripts]
                               canossa = canossa:main
