9035d4fe5020706e6e63555d287fa3ec7ec2a639
saitoha <user@zuse.jp>
2012-11-27 20:54:30 +0900

    Add line tests

--- canossa/line.py
+++ canossa/line.py
@@ -21,73 +21,82 @@
 from attribute import Attribute
 from cell import Cell
 
-""" 
-This module exports Line object, that consists of some cell objects.
-
-   +-----+-----+-----+-...                   ...-+-----+
-   |  A  |  B  |  C  |       ... ... ...         |     |
-   +-----+-----+-----+-...                   ...-+-----+
-
-Ordinally, each cell contains a narrow character
-
->>> line = Line(10)
->>> attr = Attribute()
->>> print line
-<ESC>#5<ESC>(B<ESC>[0m<SP><SP><SP><SP><SP><SP><SP><SP><SP><SP>
->>> line.clear(attr)
->>> print line
-<ESC>#5<ESC>(B<ESC>[0;39;49m<SP><SP><SP><SP><SP><SP><SP><SP><SP><SP>
->>> line.write(0x40, 0, attr)
->>> line.write(0x50, 0, attr)
->>> print line
-<ESC>#5<ESC>(B<ESC>[0;39;49mP<SP><SP><SP><SP><SP><SP><SP><SP><SP>
->>> line.write(0x40, 1, attr)
->>> print line
-<ESC>#5<ESC>(B<ESC>[0;39;49mP@<SP><SP><SP><SP><SP><SP><SP><SP>
-
-A wide character occupies 2 cells, the first cell contains '\0'
-
-   +----+---------+-----+-...                   ...-+-----+
-   | \0 | \x3042  |  C  |       ... ... ...         |     |
-   +----+---------+-----+-...                   ...-+-----+
-    <------------> <--->
-     a wide char   char 
-
->>> line.write(0x3042, 2, attr)
->>> print line
-<ESC>#5<ESC>(B<ESC>[0;39;49mP@あ<SP><SP><SP><SP><SP><SP><SP>
->>> line.write(0x30, 5, attr)
->>> print line
-<ESC>#5<ESC>(B<ESC>[0;39;49mP@あ<SP><SP>0<SP><SP><SP><SP>
-"""
 
 _LINE_TYPE_DHLT = 3
 _LINE_TYPE_DHLB = 4
 _LINE_TYPE_SWL  = 5
 _LINE_TYPE_DWL  = 6
 
+'''
+    This module exports Line object, that consists of some cell objects.
+    
+       +-----+-----+-----+-...                   ...-+-----+
+       |  A  |  B  |  C  |       ... ... ...         |     |
+       +-----+-----+-----+-...                   ...-+-----+
+    
+    Ordinally, each cell contains a narrow character
+    
+    A wide character occupies 2 cells, the first cell contains '\0'
+    
+       +----+---------+-----+-...                   ...-+-----+
+       | \0 | \x3042  |  C  |       ... ... ...         |     |
+       +----+---------+-----+-...                   ...-+-----+
+        <------------> <--->
+         a wide char   char 
+'''
+
 class SupportsDoubleSizedTrait():
-    ''' For DECDWL/DECDHL support '''
+    ''' For DECDWL/DECDHL support
+    '''
 
     _type = _LINE_TYPE_SWL
 
     def set_swl(self):
+        '''
+        >>> line = Line(5)
+        >>> line.set_swl() 
+        >>> line.type() == _LINE_TYPE_SWL
+        True
+        '''
         self._type = _LINE_TYPE_SWL
         self.dirty = True
 
     def set_dwl(self):
+        '''
+        >>> line = Line(5)
+        >>> line.set_dwl() 
+        >>> line.type() == _LINE_TYPE_DWL
+        True
+        '''
         self._type = _LINE_TYPE_DWL
         self.dirty = True
 
     def set_dhlt(self):
+        '''
+        >>> line = Line(5)
+        >>> line.set_dhlt() 
+        >>> line.type() == _LINE_TYPE_DHLT
+        True
+        '''
         self._type = _LINE_TYPE_DHLT
         self.dirty = True
 
     def set_dhlb(self):
+        '''
+        >>> line = Line(5)
+        >>> line.set_dhlb() 
+        >>> line.type() == _LINE_TYPE_DHLB
+        True
+        '''
         self._type = _LINE_TYPE_DHLB
         self.dirty = True
 
     def type(self):
+        '''
+        >>> line = Line(5)
+        >>> line.type() == _LINE_TYPE_SWL
+        True
+        '''
         return self._type
 
 class SupportsWideTrait():
@@ -100,6 +109,15 @@ class SupportsCombiningTrait():
     ''' provides combine method. it combines specified character to the cell at specified position. '''
 
     def combine(self, value, pos):
+        '''
+        >>> line = Line(5)
+        >>> attr = Attribute()
+        >>> line.clear(attr)
+        >>> line.write(0x40, 1, attr)
+        >>> line.combine(0x300, 2)
+        >>> print line
+        <ESC>#5<ESC>(B<ESC>[0;39;49m<SP>@̀<SP><SP><SP>
+        '''
         self.cells[max(0, pos - 1)].combine(value)
 
 class Line(SupportsDoubleSizedTrait,
@@ -107,13 +125,39 @@ class Line(SupportsDoubleSizedTrait,
            SupportsCombiningTrait):
 
     def __init__(self, width):
+        '''
+        >>> line = Line(10)
+        >>> len(line.cells)
+        10
+        >>> line.dirty
+        True
+        '''
         self.cells = [Cell() for cell in xrange(0, width)]
         self.dirty = True
 
     def length(self):
+        '''
+        >>> line = Line(19)
+        >>> line.length()
+        19
+        '''
         return len(self.cells)
 
     def resize(self, col):
+        '''
+        >>> line = Line(14)
+        >>> line.length()
+        14
+        >>> line.resize(9)
+        >>> line.length()
+        9
+        >>> line.resize(0)
+        >>> line.length()
+        0
+        >>> line.resize(20)
+        >>> line.length()
+        20
+        '''
         width = len(self.cells)
         if col < width:
             self.cells = self.cells[:col]
@@ -122,15 +166,45 @@ class Line(SupportsDoubleSizedTrait,
         self.dirty = True
 
     def clear(self, attr):
+        '''
+        >>> line = Line(5)
+        >>> line.clear(Attribute())
+        >>> print line
+        <ESC>#5<ESC>(B<ESC>[0;39;49m<SP><SP><SP><SP><SP>
+        '''
         self.dirty = True
         self._type = _LINE_TYPE_SWL
         for cell in self.cells:
             cell.clear(attr)
 
     def write(self, value, pos, attr):
+        '''
+        >>> line = Line(5)
+        >>> attr = Attribute()
+        >>> line.clear(attr)
+        >>> line.write(0x40, 0, attr)
+        >>> print line
+        <ESC>#5<ESC>(B<ESC>[0;39;49m@<SP><SP><SP><SP>
+        >>> line.write(0x50, 0, attr)
+        >>> print line
+        <ESC>#5<ESC>(B<ESC>[0;39;49mP<SP><SP><SP><SP>
+        >>> line.write(0x3042, 1, attr)
+        >>> print line
+        <ESC>#5<ESC>(B<ESC>[0;39;49mPあ<SP><SP><SP>
+        '''
         self.cells[pos].write(value, attr)
 
     def draw(self, s, left, right):
+        '''
+        >>> line = Line(5)
+        >>> import StringIO
+        >>> s = StringIO.StringIO()
+        >>> line.draw(s, 3, 5)
+        >>> result = s.getvalue().replace("\x1b", "<ESC>")
+        >>> result = result.replace("\x20", "<SP>")
+        >>> print result 
+        <ESC>#5<ESC>(B<ESC>[0m<SP><SP>
+        '''
         self.dirty = False
         attr = None 
         cells = self.cells
@@ -159,6 +233,11 @@ class Line(SupportsDoubleSizedTrait,
                     break
 
     def __str__(self):
+        '''
+        >>> line = Line(5)
+        >>> print line 
+        <ESC>#5<ESC>(B<ESC>[0m<SP><SP><SP><SP><SP>
+        '''
         import StringIO, codecs
         import locale
         language, encoding = locale.getdefaultlocale()
@@ -167,8 +246,35 @@ class Line(SupportsDoubleSizedTrait,
         result = s.getvalue().replace("\x1b", "<ESC>")
         result = result.replace("\x20", "<SP>")
         result = result.replace("\x00", "<NUL>")
+        return result
 
-if __name__ == "__main__":
+def test():
+    """ 
+    >>> line = Line(10)
+    >>> attr = Attribute()
+    >>> print line
+    <ESC>#5<ESC>(B<ESC>[0m<SP><SP><SP><SP><SP><SP><SP><SP><SP><SP>
+    >>> line.clear(attr)
+    >>> print line
+    <ESC>#5<ESC>(B<ESC>[0;39;49m<SP><SP><SP><SP><SP><SP><SP><SP><SP><SP>
+    >>> line.write(0x40, 0, attr)
+    >>> line.write(0x50, 0, attr)
+    >>> print line
+    <ESC>#5<ESC>(B<ESC>[0;39;49mP<SP><SP><SP><SP><SP><SP><SP><SP><SP>
+    >>> line.write(0x40, 1, attr)
+    >>> print line
+    <ESC>#5<ESC>(B<ESC>[0;39;49mP@<SP><SP><SP><SP><SP><SP><SP><SP>
+    >>> line.pad(2)
+    >>> line.write(0x3042, 3, attr)
+    >>> print line
+    <ESC>#5<ESC>(B<ESC>[0;39;49mP@あ<SP><SP><SP><SP><SP><SP>
+    >>> line.write(0x30, 5, attr)
+    >>> print line
+    <ESC>#5<ESC>(B<ESC>[0;39;49mP@あ<SP>0<SP><SP><SP><SP>
+    """
     import doctest
     doctest.testmod()
+
+if __name__ == "__main__":
+    test()
 
