287f4ec17a86d508b30bf188600c832c8d444d98
saitoha <user@zuse.jp>
2012-12-05 20:40:53 +0900

    Fix setup.py: read rst with absolute path

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@
 from setuptools import setup, find_packages
 from canossa import __version__, __license__, __author__
 
+import inspect, os
+
+filename = inspect.getfile(inspect.currentframe())
+dirpath = os.path.abspath(os.path.dirname(inspect.getfile(inspect.currentframe())))
+
 import canossa.line
 canossa.line.test()
 
 setup(name                  = 'canossa',
       version               = __version__,
       description           = 'Provides basic, transparent, off-screen(invisible) terminal emulation service, for terminal apps.',
-      long_description      = open("README.rst").read(),
+      long_description      = open(dirpath + "/README.rst").read(),
       py_modules            = ['canossa'],
       eager_resources       = [],
       classifiers           = ['Development Status :: 4 - Beta',
@@ -27,7 +32,8 @@ setup(name                  = 'canossa',
       packages              = find_packages(exclude=[]),
       zip_safe              = True,
       include_package_data  = False,
-      install_requires      = ['tff >=0.0.14, <0.1.0'],
+#      install_requires      = ['tff >=0.0.14, <0.1.0', 'termprop==0.0.1'],
+      install_requires      = [],
       entry_points          = """
                               [console_scripts]
                               canossa = canossa:main
