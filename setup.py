1cf9fc9c4a404df226687fa768b3533e4e0d1516
Hayaki Saito <user@zuse.jp>
2014-01-03 21:57:46 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(name                  = 'canossa',
                                'Topic :: Terminals',
                                'Environment :: Console',
                                'Intended Audience :: Developers',
-                               'License :: OSI Approved :: GNU General Public License (GPL)',
+                               'License :: OSI Approved :: MIT License',
                                'Programming Language :: Python'
                                ],
       keywords              = 'terminal',
